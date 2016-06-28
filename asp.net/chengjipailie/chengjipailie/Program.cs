909bee476093301bf992c3c81a1cdef2cf186e45
knightgao <knight_gao@hotmail.com>
2016-06-28 11:18:21 +0800

    Pat4

---
+++ asp.net/chengjipailie/chengjipailie/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace chengjipailie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Int32.Parse(Console.ReadLine());
+            string[] name = new string[n];
+            string[] XH = new string[n];
+            int[] CJ = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                string strA = Console.ReadLine();
+                char[] Sep = { ' ' };
+                String[] strB = new String[3];
+                strB = strA.Split(Sep);
+                name[i] = strB[0];
+                XH[i] = strB[1];
+                CJ[i] = Int32.Parse(strB[2]);
+            }
+            for (int i = 0; i < CJ.Length; i++)
+            {
+
+            }
+        }
+    }
+}
