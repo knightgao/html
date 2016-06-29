ab5c8b18075a66be0777cb328e026960380e1439
knightgao <knight_gao@hotmail.com>
2016-06-29 09:03:16 +0800

    fullname

---
+++ asp.net/fullname/fullname/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Fullname
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName;
+            string lastName;
+            string fullName;
+
+            Console.WriteLine("het you !");
+
+            firstName = GetUserInput("Enter your first name: ");
+            lastName = GetUserInput("Enter your last name: ");
+            fullName = GetFullName(firstName, lastName);
+            DispalyGreeting(fullName);
+        }
+        static string GetUserInput(string prompt)
+        {
+            Console.Write(prompt);
+            return Console.ReadLine();
+        }
+        static string GetFullName(string firstName, string lastName)
+        { 
+            return string.Format("{0} {1}",firstName,lastName);
+        }
+        static void DispalyGreeting(string name)
+        {
+            Console.WriteLine("Your full name is {0}.", name);
+        }
+    }
+}
